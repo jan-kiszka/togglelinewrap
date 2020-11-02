3a870bc29186f822eb3b38960de74573f234357c
Jan Kiszka <jan.kiszka@web.de>
2020-11-02 07:46:01 +0100

    Reformat
    For better readability. No functional changes.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@web.de>

--- background-script.js
+++ background-script.js
@@ -13,76 +13,77 @@
 
 async function updateComposeAction(tab, defaultWidth)
 {
-  if (defaultWidth === 0) {
-    messenger.composeAction.disable(tab.id);
-    return;
-  }
+    if (defaultWidth === 0) {
+        messenger.composeAction.disable(tab.id);
+        return;
+    }
 
-  let width = await messenger.composeLineWrap.getEditorWrapWidth(tab.windowId);
-  messenger.composeAction.setBadgeText({
-    tabId: tab.id,
-    text: (width === 0) ? "off" : ""
-  });
-  messenger.composeAction.enable(tab.id);
+    let width = await messenger.composeLineWrap.getEditorWrapWidth(tab.windowId);
+    messenger.composeAction.setBadgeText({
+        tabId: tab.id,
+        text: (width === 0) ? "off" : ""
+    });
+    messenger.composeAction.enable(tab.id);
 }
 
 async function toggleLineWrap(tab)
 {
-  let defaultWidth = await messenger.composeLineWrap.getDefaultWrapWidth(tab.windowId);
-  if (defaultWidth === 0) {
-    return;
-  }
+    let defaultWidth = await messenger.composeLineWrap.getDefaultWrapWidth(tab.windowId);
+    if (defaultWidth === 0) {
+        return;
+    }
 
-  let width = await messenger.composeLineWrap.getEditorWrapWidth(tab.windowId);
-  if (width > 0) {
-    width = 0;
-  } else {
-    width = defaultWidth;
-  }
-  messenger.composeLineWrap.setEditorWrapWidth(tab.windowId, width);
+    let width = await messenger.composeLineWrap.getEditorWrapWidth(tab.windowId);
+    if (width > 0) {
+        width = 0;
+    } else {
+        width = defaultWidth;
+    }
+    messenger.composeLineWrap.setEditorWrapWidth(tab.windowId, width);
 
-  updateComposeAction(tab, defaultWidth);
+    updateComposeAction(tab, defaultWidth);
 }
 
 async function updateWindow(windowId)
 {
-  let window = await messenger.windows.get(windowId);
+    let window = await messenger.windows.get(windowId);
 
-  if (window.type === "messageCompose") {
-    let defaultWidth = await messenger.composeLineWrap.getDefaultWrapWidth(window.id);
-    messenger.tabs.query({windowId: window.id}).then(tabs => {;
-      updateComposeAction(tabs[0], defaultWidth);
-    });
-  }
+    if (window.type === "messageCompose") {
+        let defaultWidth = await messenger.composeLineWrap.getDefaultWrapWidth(window.id);
+        messenger.tabs.query({windowId: window.id}).then(tabs => {;
+            updateComposeAction(tabs[0], defaultWidth);
+        });
+    }
 }
 
-async function main() {
-  messenger.composeAction.disable();
+async function main()
+{
+    messenger.composeAction.disable();
 
-  messenger.windows.onFocusChanged.addListener(windowId => {
-    if (windowId !== messenger.windows.WINDOW_ID_NONE) {
-      updateWindow(windowId);
-    }
-  });
-
-  messenger.commands.onCommand.addListener(name => {
-    if (name === "toggleLineWrap") {
-      messenger.windows.getAll().then(windows => {
-        for (let window of windows) {
-          if (window.type === "messageCompose" && window.focused === true) {
-            messenger.tabs.query({windowId: window.id}).then(tabs => {
-              toggleLineWrap(tabs[0]);
-            });
-            break;
-          }
+    messenger.windows.onFocusChanged.addListener(windowId => {
+        if (windowId !== messenger.windows.WINDOW_ID_NONE) {
+            updateWindow(windowId);
         }
-      });
-    }
-  });
+    });
 
-  messenger.composeAction.onClicked.addListener(tab => {
-    toggleLineWrap(tab);
-  });
+    messenger.commands.onCommand.addListener(name => {
+        if (name === "toggleLineWrap") {
+            messenger.windows.getAll().then(windows => {
+                for (let window of windows) {
+                    if (window.type === "messageCompose" && window.focused === true) {
+                        messenger.tabs.query({windowId: window.id}).then(tabs => {
+                            toggleLineWrap(tabs[0]);
+                        });
+                        break;
+                    }
+                }
+            });
+        }
+    });
+
+    messenger.composeAction.onClicked.addListener(tab => {
+        toggleLineWrap(tab);
+    });
 }
 
 main();
--- composeLineWrapAPI.js
+++ composeLineWrapAPI.js
@@ -16,45 +16,45 @@ var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
 
 function getMsgCompose(windowId)
 {
-  let window = Services.wm.getOuterWindowWithId(windowId);
-  return window.document.defaultView.gMsgCompose;
+    let window = Services.wm.getOuterWindowWithId(windowId);
+    return window.document.defaultView.gMsgCompose;
 }
 
 function getDefaultWidth(windowId)
 {
-  let gMsgCompose = getMsgCompose(windowId);
+    let gMsgCompose = getMsgCompose(windowId);
 
-  return gMsgCompose.composeHTML ? 0 : gMsgCompose.wrapLength;
+    return gMsgCompose.composeHTML ? 0 : gMsgCompose.wrapLength;
 }
 
 function setWrapWidth(windowId, value)
 {
-  let gMsgCompose = getMsgCompose(windowId);
+    let gMsgCompose = getMsgCompose(windowId);
 
-  gMsgCompose.editor.wrapWidth = value;
+    gMsgCompose.editor.wrapWidth = value;
 }
 
 function getWrapWidth(windowId)
 {
-  let gMsgCompose = getMsgCompose(windowId);
+    let gMsgCompose = getMsgCompose(windowId);
 
-  return gMsgCompose.editor ? gMsgCompose.editor.wrapWidth : getDefaultWidth(windowId);
+    return gMsgCompose.editor ? gMsgCompose.editor.wrapWidth : getDefaultWidth(windowId);
 }
 
 var composeLineWrap = class extends ExtensionCommon.ExtensionAPI {
-  getAPI(context) {
-    return {
-      composeLineWrap: {
-        async setEditorWrapWidth(windowId, value) {
-          setWrapWidth(windowId, value);
-        },
-        async getEditorWrapWidth(windowId) {
-          return getWrapWidth(windowId);
-        },
-        async getDefaultWrapWidth(windowId) {
-          return getDefaultWidth(windowId);
+    getAPI(context) {
+        return {
+            composeLineWrap: {
+                async setEditorWrapWidth(windowId, value) {
+                    setWrapWidth(windowId, value);
+                },
+                async getEditorWrapWidth(windowId) {
+                    return getWrapWidth(windowId);
+                },
+                async getDefaultWrapWidth(windowId) {
+                    return getDefaultWidth(windowId);
+                }
+            }
         }
-      }
     }
-  }
 };
